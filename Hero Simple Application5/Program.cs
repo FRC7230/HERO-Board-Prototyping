580294c3bdf5690d0c5e39b05b6144eef39ba660
Shirley Shah <shirleyshah1002@gmail.com>
2018-12-08 13:39:03 -0800

    Added motor control

--- Hero Simple Application5/Program.cs
+++ Hero Simple Application5/Program.cs
@@ -8,11 +8,23 @@ namespace Hero_Simple_Application5
     
     public class Program
     {
-                  static CTRE.Phoenix.Controller.GameController _gamepad = new CTRE.Phoenix.Controller.GameController(CTRE.Phoenix.UsbHostDevice.GetInstance(0));
-
+                    static CTRE.Phoenix.Controller.GameController _gamepad = new CTRE.Phoenix.Controller.GameController(CTRE.Phoenix.UsbHostDevice.GetInstance(0));
+                    static CTRE.Phoenix.MotorControl.CAN.TalonSRX Talon1 = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(1);
+        static CTRE.Phoenix.MotorControl.CAN.TalonSRX Talon2 = new CTRE.Phoenix.MotorControl.CAN.TalonSRX(2);
         public static void Main()
         {
-           
+
+            Talon1.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, _gamepad.GetAxis(1)+_gamepad.GetAxis(2));
+            Talon2.Set(CTRE.Phoenix.MotorControl.ControlMode.PercentOutput, _gamepad.GetAxis(1)+_gamepad.GetAxis(2));
+            
+
+            uint period = 50000; //period between pulses
+            uint duration = 1500; //duration of pulse
+            PWM pwm_9 = new PWM(CTRE.HERO.IO.Port3.PWM_Pin9, period, duration,
+            PWM.ScaleFactor.Microseconds, false);
+
+            pwm_9.Start();
+        
 
             OutputPort solonoid_extend = new OutputPort(CTRE.HERO.IO.Port5.Pin4, false);
             OutputPort solonoid_retract = new OutputPort(CTRE.HERO.IO.Port5.Pin6, false);
@@ -23,6 +35,8 @@ namespace Hero_Simple_Application5
             bool bButton = true;
             while(true)
                 {
+                pwm_9.Duration = (uint)_gamepad.GetAxis(1) * 50000;
+
                 xButton = _gamepad.GetButton(1);
                 aButton = _gamepad.GetButton(2);
                 bButton = _gamepad.GetButton(3);
